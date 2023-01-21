b198c5494a0f556e78b7ed5c9e9897e740a0f0ad
Renata Nogueira <renata86ti@gmail.com>
2023-01-21 11:37:54 -0300

    first commit

---
+++ Criança/Adolescente/Adulto/index.js
@@ -0,0 +1,25 @@
+// var nan = 'gastei 10' - 5;
+// var nan2 = '10' + '10';
+
+// var somastring = +'90' + 10;
+
+// var numero = '100';
+// var kg = 'kg';
+// var peso = numero + kg;
+// var peso2 = numero / 2 + kg;
+
+// var incremento = 10
+// console.log(++incremento)
+const maiorIdade = 12
+function promptMe(){
+    var idade = prompt("Qual sua idade?");
+    if (idade >= 18){  alert('Adulto')} 
+    
+    else if (idade>=14) {alert('adolecente')}
+
+    else {alert('Criança')}   
+    }
+console.log(idade)
+
+
+
---
+++ Maior/menor de idade/index2.js
@@ -0,0 +1,21 @@
+// var nan = 'gastei 10' - 5;
+// var nan2 = '10' + '10';
+
+// var somastring = +'90' + 10;
+
+// var numero = '100';
+// var kg = 'kg';
+// var peso = numero + kg;
+// var peso2 = numero / 2 + kg;
+
+// var incremento = 10
+// console.log(++incremento)
+const maiorIdade = 12
+function promptMe(){
+    var idade = prompt("Qual sua idade?");
+    if (idade >= 18){  alert('Maior de idade')} else {alert('Menor de idade')}   
+    }
+console.log(idade)
+
+
+
